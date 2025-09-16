70b0dd5b100ae25d87daaee8ecbdc88511265368
unknown <leonardosolis752@gmail.com>
2025-09-16 00:42:50 -0500

    chore: initial commit

---
+++ src/main/java/com/upc/quadrapp/QuadrappApplication.java
@@ -0,0 +1,13 @@
+package com.upc.quadrapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadrappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadrappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/quadrapp/QuadrappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.quadrapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadrappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
